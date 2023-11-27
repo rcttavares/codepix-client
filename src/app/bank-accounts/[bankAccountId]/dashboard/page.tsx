f55682e2bbec275e67177213efe078484e6a15bf
Rafael Tavares <rafael.tawares@gmail.com>
2023-11-27 20:57:22 -0300

    Create transactions

--- src/app/bank-accounts/[bankAccountId]/dashboard/page.tsx
+++ src/app/bank-accounts/[bankAccountId]/dashboard/page.tsx
@@ -1,13 +1,32 @@
 import { CardAction } from '@/components/CardAction';
 import { CurrentBalance } from '@/components/CurrentBalance';
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+import { MyLatestTransactions } from './MyLatestTransactions';
+import { Transaction } from '@/models';
 import { Typography } from '@mui/material';
 
-export function BankAccountDashboardPage({
+export async function getTransactions(
+  bankAccountId: string
+): Promise<Transaction[]> {
+  const response = await fetch(
+    `http://host.docker.internal:3000/bank-accounts/${bankAccountId}/transactions`,
+    {
+      next: {
+        revalidate: 10,
+      },
+    }
+  );
+
+  return response.json();
+}
+
+export async function BankAccountDashboardPage({
   params,
 }: {
   params: { bankAccountId: string };
 }) {
+  const transactions = await getTransactions(params.bankAccountId);
+
   return (
     <Grid2 container spacing={2}>
       <Grid2 xs={12} lg={6}>
@@ -36,6 +55,7 @@ export function BankAccountDashboardPage({
 
       <Grid2 xs={12}>
         <Typography variant='h5'>Útimos lançamentos</Typography>
+        <MyLatestTransactions transactions={transactions} />
       </Grid2>
     </Grid2>
   );
